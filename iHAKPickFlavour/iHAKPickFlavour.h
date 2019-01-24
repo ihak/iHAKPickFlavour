5b3a946e15d7e17f903bc0467107d5315a381b50
Hassan Ahmed <hassan.ahmed@arpatech.com>
2019-01-24 15:48:38 +0500

    Initial commit

---
+++ iHAKPickFlavour/iHAKPickFlavour.h
@@ -0,0 +1,19 @@
+//
+//  iHAKPickFlavour.h
+//  iHAKPickFlavour
+//
+//  Created by Hassan Ahmed on 24/01/2019.
+//  Copyright © 2019 Hassan Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iHAKPickFlavour.
+FOUNDATION_EXPORT double iHAKPickFlavourVersionNumber;
+
+//! Project version string for iHAKPickFlavour.
+FOUNDATION_EXPORT const unsigned char iHAKPickFlavourVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iHAKPickFlavour/PublicHeader.h>
+
+
